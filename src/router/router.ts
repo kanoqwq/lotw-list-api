edf29ae89e054b89cbb279c6c1fb27d0bd59c089
kanoqwq <kanoqwq@qq.com>
2023-08-14 13:56:42 +0800

    modified:exportFile api path

--- src/router/router.ts
+++ src/router/router.ts
@@ -7,7 +7,7 @@ router.get('/lotw', Controller.getQsos)
 //展示详细信息
 router.get('/lotw/qsldetails', Controller.getQSLDetails)
 //导出数据
-router.get('/downloadfile', Controller.exportFile)
+router.get('/lotw/downloadfile', Controller.exportFile)
 //登录
 // router.post('/login')
 //登出
